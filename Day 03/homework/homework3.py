f7a251864d9423ccb8852a05c3876f267f639696
GiorgiOsefashvili <gioosefashvili3@gmail.com>
2024-03-07 13:43:53 +0300

    fourth commit

---
+++ Day 03/homework/homework3.py
@@ -0,0 +1,26 @@
+print("wake up")
+print(blabla)
+print gsdf
+print blabla
+print  fsad
+printblablad)
+printblabla)
+printblabla)
+print(blabla
+print fsdf
+print sdsdsd        
+print sdsdds
+print dasda
+print("wake up")
+print(blabla)
+print gsdf
+print blabla
+print  fsad
+printblablad)
+printblabla)
+printblabla)
+print(blabla
+print fsdf
+print sdsdsd        
+print sdsdds
+print dasda
---
+++ Day 03/lesson/lesson3.py
@@ -0,0 +1,13 @@
+mom_name = "Liza"
+
+dad_name = "Zura"
+
+sister_name = "Ani"
+
+brother_name = "tornike"
+
+sister2_name = "taia"
+
+print("my mom's name is", mom_name, "my dad's name is",dad_name,
+"my sister's name is",sister_name,"my second sister's name is",sister2_name,
+"my brother's name is",brother_name)
